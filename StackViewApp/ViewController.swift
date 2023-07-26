ee08bc45d8c7ec98b8a8d5406d10c3ab810b9a68
ing911 <axmed.xamchiev@gmail.com>
2023-07-26 21:05:54 +0300

    Removed template comments Удалил шаблонные комментарии Неправильно сделал предыдущий коммит (был сделан main.storyboard, добавлены и настроены два stack view) Не знаю как исправить уже сделанный коммит (пока что)

--- StackViewApp/ViewController.swift
+++ StackViewApp/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  StackViewApp
-//
-//  Created by Пользователь on 26.07.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
